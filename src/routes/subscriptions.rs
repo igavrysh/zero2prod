422bf6077b43ede2e61f152eb54464924a6ba68b
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-08-26 03:58:27 -0700

    email server business logic

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -3,7 +3,7 @@ use chrono::Utc;
 use uuid::Uuid;
 use sqlx::PgPool;
 
-use crate::domain::{NewSubscriber, SubscriberName, SubscriberEmail};
+use crate::{domain::{NewSubscriber, SubscriberName, SubscriberEmail}, email_client::EmailClient};
 
 #[derive(serde::Deserialize)]
 #[warn(dead_code)]
@@ -24,7 +24,7 @@ impl TryFrom<FormData> for NewSubscriber {
 
 #[tracing::instrument(
     name = "Adding a new subscriber",
-    skip(form, pool),
+    skip(form, pool, email_cleint),
     fields(
         subcriber_email = %form.email,
         subcriber_name = %form.name
@@ -33,16 +33,31 @@ impl TryFrom<FormData> for NewSubscriber {
 pub async fn subscribe(
     form: web::Form<FormData>,
     pool: web::Data<PgPool>,
+    email_cleint: web::Data<EmailClient>,
 ) -> HttpResponse {
     let new_subscriber =  match form.0.try_into() {
         Ok(form) => form,
         Err(_) => return HttpResponse::BadRequest().finish(),
     };
 
-    match insert_subscriber(&pool, &new_subscriber).await {
-        Ok(_) => HttpResponse::Ok().finish(),
-        Err(_) => HttpResponse::InternalServerError().finish()
+    if insert_subscriber(&pool, &new_subscriber).await.is_err() {
+        return HttpResponse::InternalServerError().finish();
     }
+
+    if email_cleint
+        .send_email(
+            new_subscriber.email, 
+            "Welcome!", 
+            "Welcome to our newsletter!", 
+            "Welcome to our newsletter!",
+        )
+        .await
+        .is_err()
+    {
+        return HttpResponse::InternalServerError().finish()
+    }
+
+    HttpResponse::Ok().finish()
 }
 
 #[tracing::instrument(
--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,6 +1,7 @@
 use once_cell::sync::Lazy;
 use sqlx::{PgConnection, Connection, PgPool, Executor};
 use uuid::Uuid;
+use wiremock::MockServer;
 use zero2prod::configuration::{get_configuration, DatabaseSettings};
 use zero2prod::startup::{get_connection_pool, Application};
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
@@ -20,6 +21,7 @@ static TRACING: Lazy<()> = Lazy::new(|| {
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
+    pub email_server: MockServer,
 }
 
 impl TestApp {
@@ -37,6 +39,9 @@ impl TestApp {
 pub async fn spawn_app() -> TestApp {
     Lazy::force(&TRACING);
 
+    // Launch a mock server to stand in for Postmark's API
+    let email_server = MockServer::start().await;
+
     // Randomise configuration to ensure test isolation
     let configuration = {
         let mut c = get_configuration().expect("Failed to read configuration.");
@@ -44,6 +49,9 @@ pub async fn spawn_app() -> TestApp {
         c.database.database_name = Uuid::new_v4().to_string();
         // Use a random OS port
         c.application.port = 0;
+
+        // Use the mock server as email API
+        c.email_client.base_url = email_server.uri();
         c
     };
 
@@ -62,6 +70,7 @@ pub async fn spawn_app() -> TestApp {
     TestApp {
         address: address,
         db_pool: get_connection_pool(&configuration.database),
+        email_server,
     }
 }
 
