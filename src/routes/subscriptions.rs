20b3837ee1db29a3b2756f69763633c92d8e69e6
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-09-02 12:01:41 -0700

    further errors refactoring

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -52,21 +52,37 @@ pub async fn subscribe(
     let mut transaction = pool
         .begin()
         .await
-        .map_err(|e| SubscribeError::UnexpectedError(Box::new(e)))?;
+        .map_err(|e| SubscribeError::UnexpectedError(
+            Box::new(e), 
+            "Failed to acquire a Postgres connection from the pool".into(),
+        )
+    )?;
 
     let subscriber_id = insert_subscriber(&mut transaction, &new_subscriber)
         .await
-        .map_err(|e| SubscribeError::UnexpectedError(Box::new(e)))?;
+        .map_err(|e| SubscribeError::UnexpectedError(
+            Box::new(e),
+            "Failed to insert new subscriber in the database".into(),
+        )
+    )?;
 
     let subscription_token = generate_subscription_token();
 
     store_token(&mut transaction, subscriber_id, &subscription_token)
         .await
-        .map_err(|e| SubscribeError::UnexpectedError(Box::new(e)))?;
+        .map_err(|e| SubscribeError::UnexpectedError(
+            Box::new(e),
+            "Failed to store the confirmation token for a new subscriber".into()
+        )
+    )?;
 
     transaction.commit()
         .await
-        .map_err(|e| SubscribeError::UnexpectedError(Box::new(e)))?;
+        .map_err(|e| SubscribeError::UnexpectedError(
+            Box::new(e),
+            "Failed to commit SQL transaction to store a new subscriber".into()
+        )
+    )?;
 
     send_confirmation_email(
         &email_cleint, 
@@ -74,7 +90,11 @@ pub async fn subscribe(
         &base_url.0,
         &subscription_token)
         .await
-        .map_err(|e| SubscribeError::UnexpectedError(Box::new(e)))?;
+        .map_err(|e| SubscribeError::UnexpectedError(
+            Box::new(e),
+            "Failed to send a confirmation email".into()
+        )
+    )?;
 
     Ok(HttpResponse::Ok().finish())
 }
@@ -201,10 +221,8 @@ pub enum SubscribeError {
     #[error("{0}")]
     ValidationError(String),
 
-    // Transparent delegate both `Display`'s and `source`'s impl to the type wrapped
-    // by `UnexpectedError`.
-    #[error(transparent)]
-    UnexpectedError(#[from] Box<dyn std::error::Error>),
+    #[error("{1}")]
+    UnexpectedError(#[source] Box<dyn std::error::Error>, String),
 }
 
 impl std::fmt::Debug for SubscribeError {
@@ -230,7 +248,7 @@ impl ResponseError for SubscribeError {
     fn status_code(&self) -> StatusCode {
         match self {
             SubscribeError::ValidationError(_) => StatusCode::BAD_REQUEST,
-            SubscribeError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            SubscribeError::UnexpectedError(_, _) => StatusCode::INTERNAL_SERVER_ERROR,
         }
     }
 }
