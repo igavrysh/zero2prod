bfaed7dac2bf819c981e70537d5c7014923cbbfc
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-09-03 20:23:13 -0700

    add basic auth checks

--- src/routes/newsletters.rs
+++ src/routes/newsletters.rs
@@ -1,6 +1,6 @@
-use actix_web::{HttpResponse, web, ResponseError, http::header::HeaderMap};
+use actix_web::{HttpResponse, web, ResponseError, http::header::HeaderMap, HttpRequest};
 use anyhow::Context;
-use reqwest::StatusCode;
+use reqwest::{StatusCode, header::{self, HeaderValue}};
 use secrecy::Secret;
 use sqlx::PgPool;
 
@@ -22,7 +22,11 @@ pub async fn publish_newsletter(
     body: web::Json<BodyData>, 
     pool: web::Data<PgPool>,
     email_client: web::Data<EmailClient>,
+    request: HttpRequest,
 ) -> Result<HttpResponse, PublishError> {
+    let credentials = basic_authentication(request.headers())
+        .map_err(PublishError::AuthError)?;
+
     let subscribers = get_confirmed_subscribers(&pool).await?;
     for subscriber in subscribers {
         match subscriber {
@@ -82,6 +86,9 @@ async fn get_confirmed_subscribers(
 
 #[derive(thiserror::Error)]
 pub enum PublishError {
+    #[error("Authentication failed.")]
+    AuthError(#[source] anyhow::Error),
+
     #[error(transparent)]
     UnexpectedError(#[from] anyhow::Error),
 }
@@ -93,9 +100,26 @@ impl std::fmt::Debug for PublishError {
 }
 
 impl ResponseError for PublishError {
+    fn error_response(&self) -> HttpResponse {
+        match self {
+            PublishError::UnexpectedError(_) => {
+                HttpResponse::new(StatusCode::INTERNAL_SERVER_ERROR)
+            }
+            PublishError::AuthError(_) => {
+                let mut response = HttpResponse::new(StatusCode::UNAUTHORIZED);
+                let header_value = HeaderValue::from_str(r#"Basic realm="publish""#)
+                    .unwrap();
+                response
+                    .headers_mut()
+                    .insert(header::WWW_AUTHENTICATE, header_value);
+                response
+            }
+        }
+    }
     fn status_code(&self) -> StatusCode {
         match self {
-            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR
+            PublishError::UnexpectedError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            PublishError::AuthError(_) => StatusCode::UNAUTHORIZED,
         }
     }
 }
@@ -105,8 +129,6 @@ struct Credentials {
     password: Secret<String>,
 }
 
-/* 
-
 fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Error> {
     let header_value = headers
         .get("Authorization")
@@ -115,8 +137,24 @@ fn basic_authentication(headers: &HeaderMap) -> Result<Credentials, anyhow::Erro
         .context("The 'Authorization' header was not a valid UTF8 string.")?;
     let base64encoded_segment = header_value
         .strip_prefix("Basic ")
-        .context("The authorization scheme was not 'Basic'.")?
+        .context("The authorization scheme was not 'Basic'.")?;
+    let decoded_bytes = base64::decode_config(base64encoded_segment, base64::STANDARD)
+        .context("Failed to base64-decode 'Basic credentials.")?;
+    let decoded_credentials = String::from_utf8(decoded_bytes)?;
+
+    let mut credentials = decoded_credentials.splitn(2, ":");
+    let username = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("A usename must be provided in 'Basic' auth."))?
+        .to_string();
+    let password = credentials
+        .next()
+        .ok_or_else(|| anyhow::anyhow!("A password must be provided in 'Basic' auth"))?
+        .to_string();
+
+    Ok(Credentials {
+        username,
+        password: Secret::new(password)
+    })
 
 }
-
-*/
