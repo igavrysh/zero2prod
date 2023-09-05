6edf3a46f7cc8290ab931dc6a3689ecaeb3d79ea
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-09-05 12:22:08 -0700

    add hmac - but failing verification

--- src/startup.rs
+++ src/startup.rs
@@ -4,6 +4,7 @@ use crate::email_client::EmailClient;
 
 use std::net::TcpListener;
 use actix_web::{dev::Server, HttpServer, web, App};
+use secrecy::Secret;
 use sqlx::PgPool;
 use sqlx::postgres::PgPoolOptions;
 use tracing_actix_web::TracingLogger;
@@ -40,6 +41,7 @@ impl Application {
             connection_pool, 
             email_client,
             configuration.application.base_url,
+            configuration.application.hmac_secret,
         )?;
 
         Ok(Self { port, server })
@@ -65,6 +67,7 @@ pub fn run(
     db_pool: PgPool,
     email_client: EmailClient,
     base_url: String,
+    hmac_secret: Secret<String>,
 ) -> Result<Server, std::io::Error> {
     let db_pool = Data::new(db_pool);
     let email_client = Data::new(email_client);
@@ -82,6 +85,7 @@ pub fn run(
             .app_data(db_pool.clone())
             .app_data(email_client.clone())
             .app_data(base_url.clone())
+            .app_data(Data::new(HmacSecret(hmac_secret.clone())))
     })
     .listen(listener)?
     .run();
@@ -96,3 +100,6 @@ pub fn get_connection_pool(
         .connect_timeout(std::time::Duration::from_secs(2))
         .connect_lazy_with(configuration.with_db())
 }
+
+#[derive(Clone)]
+pub struct HmacSecret(pub Secret<String>);
