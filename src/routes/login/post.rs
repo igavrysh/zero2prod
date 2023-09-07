0043fddcd7031481575759be339d67cd6f8181bb
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-09-07 09:38:41 -0700

    added cookies for failure message storage

--- src/routes/login/post.rs
+++ src/routes/login/post.rs
@@ -1,10 +1,9 @@
-use actix_web::{HttpResponse, web, error::InternalError};
-use hmac::{Hmac, Mac};
+use actix_web::{HttpResponse, web, error::InternalError, cookie::Cookie};
 use reqwest::header::LOCATION;
-use secrecy::{Secret, ExposeSecret};
+use secrecy::Secret;
 use sqlx::PgPool;
 
-use crate::{authentication::{Credentials, validate_credentials, AuthError}, routes::error_chain_fmt, startup::HmacSecret};
+use crate::{authentication::{Credentials, validate_credentials, AuthError}, routes::error_chain_fmt};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -13,13 +12,12 @@ pub struct FormData {
 }
 
 #[tracing::instrument(
-    skip(form, pool, secret),
+    skip(form, pool),
     fields(username=tracing::field::Empty, user_id=tracing::field::Empty)
 )]
 pub async fn login(
     form: web::Form<FormData>, 
     pool: web::Data<PgPool>,
-    secret: web::Data<HmacSecret>,
 ) -> Result<HttpResponse, InternalError<LoginError>> {
     let credentials = Credentials {
         username: form.0.username,
@@ -43,21 +41,10 @@ pub async fn login(
                     AuthError::InvalidCredentials(_) => LoginError::AuthError(e.into()),
                     AuthError::UnexpectedError(_) => LoginError::UnexpectedError(e.into()),
                 };
-                let query_string = format!(
-                    "error={}",
-                    urlencoding::Encoded::new(e.to_string())
-                );
-                let hmac_tag = {
-                    let mut mac = Hmac::<sha2::Sha256>::new_from_slice(secret.0.expose_secret().as_bytes())
-                        .unwrap();
-                    mac.update(query_string.as_bytes());
-                    mac.finalize().into_bytes()
-                };
-
                 let response = HttpResponse::SeeOther()
-                    .insert_header((LOCATION, format!("/login?{}&tag={:x}", query_string, hmac_tag)))
+                    .insert_header((LOCATION, "/login"))
+                    .cookie(Cookie::new("_flash", e.to_string()))
                     .finish();
-
                 Err(InternalError::from_response(e, response))
             }
         }
--- tests/api/login.rs
+++ tests/api/login.rs
@@ -1,4 +1,4 @@
-use crate::helpers::spawn_app;
+use crate::helpers::{spawn_app, assert_is_redirect_to};
 
 #[tokio::test]
 async fn an_error_flash_message_is_set_on_failure() {
@@ -11,4 +11,14 @@ async fn an_error_flash_message_is_set_on_failure() {
     let response = app.post_login(&login_body).await;
 
     assert_eq!(response.status().as_u16(), 303);
+    assert_is_redirect_to(&response, "/login");
+
+    let flash_cookie = response.cookies().find(|c| c.name() == "_flash").unwrap();
+    assert_eq!(flash_cookie.value(), "Authentication failed");
+
+    let html_page = app.get_login_html().await;
+    assert!(html_page.contains(r#"<p><i>Authentication failed</i></p>"#));
+
+    let html_page = app.get_login_html().await;
+    assert!(!html_page.contains(r#"<p><i>Authentication failed</i></p>"#));
 }
