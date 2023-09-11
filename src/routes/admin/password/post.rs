74357db030a6e087ae6110adeacd8aaa5e903824
Ievgen Gavrysh <ievgen.gavrysh@gmail.com>
2023-09-11 05:34:58 -0700

    remove dead code

--- src/routes/admin/password/post.rs
+++ src/routes/admin/password/post.rs
@@ -1,10 +1,8 @@
-use actix_web::{HttpResponse, web, error::InternalError};
+use actix_web::{HttpResponse, web};
 use actix_web_flash_messages::FlashMessage;
 use secrecy::{Secret, ExposeSecret};
 use sqlx::PgPool;
-use uuid::Uuid;
 use crate::{
-    session_state::TypedSession, 
     utils::{e500, see_other}, 
     routes::admin::dashboard::get_username, 
     authentication::{Credentials, validate_credentials, AuthError, UserId}
@@ -54,18 +52,4 @@ pub async fn change_password(
         .map_err(e500)?;
     FlashMessage::error("Your password has been changed.").send();
     Ok(see_other("/admin/password"))
-}
-
-
-async fn reject_anonymous_users(
-    session: TypedSession
-) -> Result<Uuid, actix_web::Error> {
-    match session.get_user_id().map_err(e500)? {
-        Some(user_id) => Ok(user_id),
-        None => {
-            let response = see_other("/login");
-            let e = anyhow::anyhow!("The user has not logged in");
-            Err(InternalError::from_response(e, response).into())
-        }
-    }
 }
